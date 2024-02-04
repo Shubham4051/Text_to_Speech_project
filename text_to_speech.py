6b8ebf29c3271126c86d94ebcb955ea08122b842
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-04 23:19:28 +0530

    playing audio directly

--- text_to_speech.py
+++ text_to_speech.py
@@ -14,7 +14,7 @@ from playsound import playsound
 print(gtts.lang.tts_langs())
 
 # Make request to google to get Synthesis
-tts = gtts.gTTS("Bhaiya Ji, Kam ho Gya Acche se kam bhi kr rha hai ", lang='hi')
+#tts = gtts.gTTS("Bhaiya Ji, Kam ho Gya Acche se kam bhi kr rha hai ", lang='hi')
 
 #1 Saving audio to a file
 #1 tts.save("hello.mp3")
@@ -25,4 +25,6 @@ tts = gtts.gTTS("Bhaiya Ji, Kam ho Gya Acche se kam bhi kr rha hai ", lang='hi')
 #9 all available languages along with their IETF tag
 #9 print(gtts.lang.tts_langs())
 
-tts.write_to_fp()
+mp3_fp = BytesIO()
+tts = gTTS('"Hello', lang='hi')
+tts.write_to_fp(mp3_fp)
