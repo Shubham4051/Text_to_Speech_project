ed360e96bbd7781d4ebbf5dfd7833a42035fc678
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-05 20:47:44 +0530

    try to advance

--- text_to_speech.py
+++ text_to_speech.py
@@ -12,18 +12,19 @@ from io import BytesIO
 from playsound import playsound
 
 # Make request to google to get Synthesis
-tts = gtts.gTTS("Bhaiya Ji, Kaam ho Gaya, Acche se kaam bhi kar raha hai ", lang='hi')
+#tts = gtts.gTTS("Bhaiya Ji, Kaam ho Gaya, Acche se kaam bhi kar raha hai ", lang='hi')
 
 #1 Saving audio to a file
-tts.save("hello.mp3")
+#tts.save("hello.mp3")
 
 #2 play the sound
-playsound("hello.mp3")
+#playsound("hello.mp3")
 
 #9 all available languages along with their IETF tag
 #9 print(gtts.lang.tts_langs())
 
 # playing audio directly
-#mp3_fp = BytesIO()
-#tts = gTTS('"Hello', lang='hi')
-#tts.write_to_fp(mp3_fp)
+mp3_fp = BytesIO()
+tts = gTTS('"Hello', lang='en')
+tts.write_to_fp(mp3_fp)
+playsound(mp3_fp)
