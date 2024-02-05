10c2aa8ef280634d2c99259f95432540973ab474
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-06 00:04:27 +0530

    success

--- text_to_speech_2.py
+++ text_to_speech_2.py
@@ -8,4 +8,28 @@
 import pyttsx3
 
 # initialize Text-to-speech engine
+
+times = int(input('Enter a number '))
 engine = pyttsx3.init()
+engine.setProperty("rate", 100)
+while times > 0:
+    times -= 1
+# convert this text to speech
+    text = input()
+    engine.say(text)
+
+
+# play the speech
+    engine.runAndWait()
+
+# get details of speaking rate
+# rate = engine.getProperty("rate")
+# print(rate)
+
+# saving speech audio into a file
+# engine.save_to_file(text, "python.mp3")
+# engine.runAndWait()
+
+#to get voice properties
+# voices = engine.getProperty("voices")
+# print(voices)
