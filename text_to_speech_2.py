2d44b8633962465ae4f6e48d4f4cf34e7c2cc550
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-05 20:53:43 +0530

    making new module for working offline as well.

---
+++ text_to_speech_2.py
@@ -0,0 +1,7 @@
+# making this for ofline modle usinf pyttsx3, main fratures
+# It works fully offline
+# You can choose among different voices that are installed on your system
+# Controlling the speed of speech
+# Tweaking volume
+# Saving the speech audio into a file.
+
