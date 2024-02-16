6a4a37f62cd134f9b6d33d5343933bb04bc8be7e
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-16 15:25:23 +0530

    retry

--- text_to_speech.py
+++ text_to_speech.py
@@ -6,10 +6,10 @@ import pyttsx3
 engine = pyttsx3.init()
 engine.setProperty("rate",150)
 
-# def text_to_speech_function():
-engine.say(input())
-engine.save_to_file(str , "real_audio.mp3")
-engine.runAndWait()
-engine.stop()
+def text_to_speech_function():
+    engine.say(input())
+    engine.save_to_file(str , "real_audio.mp3")
+    engine.runAndWait()
+    engine.stop()
 
-# text_to_speech_function()
+text_to_speech_function()
