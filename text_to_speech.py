8421863822fef19732c58ef098f07a8ce6778ba7
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-17 16:42:22 +0530

    Saved text to a .mp3 file

--- text_to_speech.py
+++ text_to_speech.py
@@ -7,8 +7,9 @@ engine = pyttsx3.init()
 engine.setProperty("rate",150)
 
 def text_to_speech_function():
-    engine.say(input())
-    # engine.save_to_file(str , "real_audio.mp3")
+    text = input("Enter your Text \n")
+    engine.say(text)
+    engine.save_to_file(text , "real_audio.mp3")
     engine.runAndWait()
     engine.stop()
 
