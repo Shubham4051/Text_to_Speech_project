d6dee478b9149bbc79b3d02e73b5f84f6b7aaf21
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-02-19 02:28:35 +0530

    try failed

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
+import uuid
+import pyttsx3
 from fastapi import FastAPI, Request
 from typing import Optional
 import pyttsx3
 from pyttsx3 import init
 # import text_to_speech
 from text_to_speech import text_to_speech_function
-
+from _TTS import _TTS
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return{"message": "Welcome to the text-to-speech API"}
+    return {"message": "Welcome to the text-to-speech API"}
 
 
 @app.post("/text_to_speech")
 def api_text_to_speech(text: str):
-    text_to_speech_function(text)
+    # text_to_speech_function(text)
+    try:
+        tts = _TTS()
+        tts.start(text)
+        del(tts)
+        return {"success"}
+    except Exception as e:
+        print(e)
 
-    return{"abcgddc"}
+
+    return {"abcgddc"}
 
 if __name__ == "__main__":
     import uvicorn
