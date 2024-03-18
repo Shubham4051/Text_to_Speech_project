8a23c3d25b8202941f250d5f6d376f0dd33d7c82
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-03-18 10:26:51 +0530

    working on it

--- Project _Main/_Speech_Engine.py
+++ Project _Main/_Speech_Engine.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import uuid
+# import pyttsx3.voice
 
 class _Speech_Engine:
     engine = None
@@ -9,6 +10,7 @@ class _Speech_Engine:
 
     def start(self,text_):
         # self.engine.setProperty(self.engine, "!v/m2")
+        self.engine.setProperty()
         self.engine.say(text_)
         file_name = (str(uuid.uuid4())) + ".mp3"
         self.engine.save_to_file(text_ , file_name)
