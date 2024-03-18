f0d50b368697bb645243e151ca9375b53f024e9e
Shubham Adarsh <shubhamadarsh6017@gmail.com>
2024-03-18 16:19:44 +0530

    testing for voice notes

--- Test.py
+++ Test.py
@@ -1,9 +1,11 @@
 import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
+print("Male Voice : {0}".format(voices[0].id))
+print("Feale Voice : {0}".format(voices[1].id))
 for voice in voices:
     print(voice, voice.id)
     engine.setProperty('voice', voice.id)
-    engine.say("Hello World!")
+    engine.say("Hello World!, Welcoe to the world of AI")
     engine.runAndWait()
-    engine.stop()
+    engine.stop()   
